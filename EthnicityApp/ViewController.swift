19f4029c559683dc98b1f0d2a7a0e04286e7aa95
Aaron Kao <aaron.kao@telus.com>
2022-03-10 18:41:51 -0800

    Initial Commit

---
+++ EthnicityApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EthnicityApp
+//
+//  Created by Aaron Kao on 2022-03-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
